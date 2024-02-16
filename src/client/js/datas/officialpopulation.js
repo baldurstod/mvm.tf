2dcbd96ec37f220c15aaebd5bb56cb7caaf5bccc
Guillaume <Guillaume>
2024-02-16 23:49:27 +0100

    Added official population

---
+++ src/client/js/datas/officialpopulation.js
@@ -0,0 +1,25 @@
+import { COMPRESSED_OFFICIAL_POPULATION } from "./compressedofficialpopulation";
+
+const OFFICIAL_POPULATION = {};
+
+export async function getPopulation(name) {
+	try {
+		let population = OFFICIAL_POPULATION[name];
+
+		if (!population) {
+			population = await decompress(COMPRESSED_OFFICIAL_POPULATION[name]);
+			OFFICIAL_POPULATION[name] = population;
+		}
+
+		return population;
+	} catch(e) {}
+}
+
+async function decompress(url) {
+	const ds = new DecompressionStream('gzip');
+	const response = await fetch(url);
+	const blobIn = await response.blob();
+	const streamIn = blobIn.stream().pipeThrough(ds);
+	const blobOut = await new Response(streamIn).blob();
+	return await blobOut.text();
+};
